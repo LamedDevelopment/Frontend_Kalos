ad8dc0fe68c205135a61c846f40da91c632f99b0
Caleb <kalebsam@hotmail.com>
2023-10-02 15:51:33 -0500

    fixes

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 
-if (environment.production) {
+/* if (environment.production) {
   enableProdMode(); // Habilita el modo de producción
   if (window) {
     window.console.log = () => {}; // Deshabilita los console.log en producción
   }
-}
+} */
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
