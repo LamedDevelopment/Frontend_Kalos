4a611a9b276ed1fa41316b4e5fa044ac5f1a9be3
LamedDevelopment <lameddev@gmail.com>
2023-12-02 01:57:54 -0500

    Configuracion puerto 3000 para el backend2

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     name:'staging',
-    // url_api: 'https://devback.bellezaap.com/api/v1/',
-    url_api: 'http://localhost:3001/api/v1/',
+    url_api: 'https://devback.bellezaap.com/api/v1/',
+    // url_api: 'http://localhost:3000/api/v1/',
 
 };
 
