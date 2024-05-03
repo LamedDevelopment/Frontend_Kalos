4e94abc6bff4f37f83494f4fecb87c3efce6ed15
kevin <kvillalbanino@gmail.com>
2024-05-02 21:42:30 -0500

    [ADD] ajuste variable de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: true,
-    name:'production',
-    url_api: 'https://devback.bellezaap.com/api/v1/',
-
+    name: 'production',
+    url_api: 'https://backalos.kalos.icu/api/v1/',
 };
-
