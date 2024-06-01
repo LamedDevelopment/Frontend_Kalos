d0ef5b39802a08208986a32675e02cad22d01cd4
LamedDevelopment <lameddev@gmail.com>
2024-05-31 19:38:51 -0500

    modified:   dist/Kalos/assets/assets/fonts/flaticon_tagus.css 	modified:   dist/Kalos/assets/assets/fonts/flaticon_tagus.html 	modified:   dist/Kalos/assets/assets/fonts/flaticon_tagus.scss 	modified:   dist/Kalos/assets/assets/fonts/flaticon_tagus.svg 	modified:   dist/Kalos/assets/fonts/flaticon_tagus.css 	modified:   dist/Kalos/assets/fonts/flaticon_tagus.html 	modified:   dist/Kalos/assets/fonts/flaticon_tagus.scss 	modified:   dist/Kalos/assets/fonts/flaticon_tagus.svg 	new file:   dist/Kalos/assets/icons/200x200.png 	new file:   dist/Kalos/assets/icons/300x300.png 	new file:   dist/Kalos/assets/icons/400x400.png 	new file:   dist/Kalos/assets/icons/50x50.png 	new file:   dist/Kalos/assets/icons/600x600.png 	new file:   dist/Kalos/assets/icons/80x80.png 	new file:   dist/Kalos/assets/icons/Kalos_Vertical.jpg 	new file:   dist/Kalos/assets/icons/Kalos_name.jpg 	new file:   dist/Kalos/assets/icons/Logo_K32x32.png 	new file:   dist/Kalos/assets/icons/Logo_K_name.png 	new file:   dist/Kalos/assets/icons/Logo_Kalos.jpg 	new file:   dist/Kalos/assets/icons/Logo_Kalos.png 	new file:   dist/Kalos/assets/icons/Logo_Kv1.png 	new file:   dist/Kalos/assets/icons/Logo_Kv2.png 	modified:   dist/Kalos/flaticon_tagus.28ed918eecbbda7a.svg 	modified:   dist/Kalos/index.html 	modified:   dist/Kalos/main.06835507ef295c05.js 	new file:   dist/Kalos/main.604f294eadabd35a.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/shared/componentsShared/sidebar/sidebar.component.html 	new file:   src/assets/icons/200x200.png 	new file:   src/assets/icons/300x300.png 	new file:   src/assets/icons/400x400.png 	new file:   src/assets/icons/50x50.png 	new file:   src/assets/icons/600x600.png 	new file:   src/assets/icons/80x80.png 	new file:   src/assets/icons/Kalos_Vertical.jpg 	new file:   src/assets/icons/Kalos_name.jpg 	new file:   src/assets/icons/Logo_K32x32.png 	new file:   src/assets/icons/Logo_K_name.png 	new file:   src/assets/icons/Logo_Kalos.jpg 	new file:   src/assets/icons/Logo_Kalos.png 	new file:   src/assets/icons/Logo_Kv1.png 	new file:   src/assets/icons/Logo_Kv2.png 	modified:   src/environments/environment.ts 	modified:   src/index.html

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
     production: false,
     name:'dev',
-    url_api: 'https://devback.bellezaap.com/api/v1/',
-    // url_api: 'http://localhost:3000/api/v1/',
+    // url_api: 'https://devback.bellezaap.com/api/v1/',
+    url_api: 'http://localhost:3000/api/v1/',
 };
 
 
