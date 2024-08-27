93091775363e1b85301452ff51e049bc943eaa47
kevin-developer <kvillalbanino@gmail.com>
2024-08-26 23:33:16 -0500

    [ADD] logica de colaborador

--- src/app/pages/common/selects/selectcollaborator/selectcollaborator.component.ts
+++ src/app/pages/common/selects/selectcollaborator/selectcollaborator.component.ts
@@ -1,71 +1,69 @@
 import {
-    ChangeDetectorRef,
-    Component,
-    EventEmitter,
-    Inject,
-    Input,
-    Output,
-    SimpleChanges,
-} from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { ModalserviceComponent } from 'src/app/pages/components/collaborator/modals/modalservice/modalservice.component';
-import { ManagerService } from 'src/app/pages/services/manager.service';
-import { CustomizerSettingsService } from 'src/app/shared/services/customizer-settings.service';
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Inject,
+  Input,
+  Output,
+  SimpleChanges,
+} from "@angular/core";
+import { FormBuilder, FormControl } from "@angular/forms";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { ModalserviceComponent } from "src/app/pages/components/collaborator/modals/modalservice/modalservice.component";
+import { ManagerService } from "src/app/pages/services/manager.service";
+import { CustomizerSettingsService } from "src/app/shared/services/customizer-settings.service";
 
 @Component({
-    selector: 'app-selectcollaborator',
-    templateUrl: './selectcollaborator.component.html',
-    styleUrls: ['./selectcollaborator.component.scss'],
+  selector: "app-selectcollaborator",
+  templateUrl: "./selectcollaborator.component.html",
+  styleUrls: ["./selectcollaborator.component.scss"],
 })
 export class SelectcollaboratorComponent {
-    dataCollaborators: any = [];
-    @Input() ctr: FormControl;
-    @Input() idbusiness: any = '';
-    @Input() services: any = '';
-    @Input() tradename: any = '';
-    @Output() collaSelected = new EventEmitter<any>();
-    optionselected = '';
-    valueInput: any;
-    constructor(private managerservice: ManagerService) {}
+  dataCollaborators: any = [];
+  @Input() ctr: FormControl;
+  @Input() idbusiness: any = "";
+  @Input() services: any = "";
+  @Input() typeService: any = "";
+  @Input() tradename: any = "";
+  @Output() collaSelected = new EventEmitter<any>();
+  optionselected = "";
+  valueInput: any;
+  constructor(private managerservice: ManagerService) {}
 
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes['services']) {
-            this.getColla();
-        }
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes["typeService"]) {
+      this.getColla();
+    }
+  }
+
+  getColla() {
+    if (
+      this.idbusiness == "" ||
+      this.services == "" ||
+      this.tradename == "" ||
+      this.typeService == ""
+    ) {
+      return;
     }
 
-    getColla() {
-        if (
-            this.idbusiness == '' ||
-            this.services == '' ||
-            this.tradename == ''
-        ) {
-            return;
-        }
-        console.log(
-            this.idbusiness,
-            this.services,
-            this.tradename,
-            'get colla'
-        );
-        let body = {
-            business: this.idbusiness,
-            services: this.services,
-            tradename: this.tradename,
-        };
-        this.managerservice
-            .getCollaborators('bus/seecolla', body)
-            .subscribe((response: any) => {
-                this.dataCollaborators = response.msg;
-            });
-    }
+    let body = {
+      business: this.idbusiness,
+      services: this.services,
+      tradename: this.tradename,
+      typeService: this.typeService,
+    };
+    this.managerservice
+      .getCollaborators("bus/seecolla", body)
+      .subscribe((response: any) => {
+        this.dataCollaborators = response.msg;
+      });
+  }
 
-    onCollaChange(event: any) {
-        this.valueInput = this.collaSelected.emit({
-            propiedad: 'business_selected',
-            valor: this.valueInput,
-        });
-        this.valueInput = this.valueInput;
-    }
+  onCollaChange(event: any) {
+    this.valueInput = this.collaSelected.emit({
+      propiedad: "business_selected",
+      valor: this.valueInput,
+    });
+    this.valueInput = this.valueInput;
+  }
 }
