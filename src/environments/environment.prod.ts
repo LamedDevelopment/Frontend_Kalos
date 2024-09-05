faab57caa503b2dcad6512a4b3bfb5fe5b12c552
Caleb <kalebsam@hotmail.com>
2024-09-05 14:00:33 -0500

    fixes variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     name: 'production',
-    url_api: 'https://beautytech.kalos.icu/api/v1/',
-    // url_api: 'https://backalos.kalos.icu/api/v1/',
+
+    url_api: 'https://backalos.kalos.icu/api/v1/',
 };
