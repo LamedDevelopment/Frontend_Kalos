75771ecc647ababc19009a8384275ac2444e298f
LAMUX <lamed.saenz@gmail.com>
2024-10-10 23:30:32 -0500

    actualizacion para login con google v2

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     name:'staging',
     url_api: 'https://devback.bellezaap.com/api/v1/',
-    clientIdQr: '171572853823-9iiq4tr1ffe40814bgbhfpothau7qbli.apps.googleusercontent.com',
+    clientIdQr: '171572853823-to9tfe6gosnlek0ervd7psc4v1s2udl0.apps.googleusercontent.com',
     clientIdLogin: '171572853823-73d98vspqp24egu8vg9no01qa55nqrhh.apps.googleusercontent.com',
     // url_api: 'http://localhost:3000/api/v1/',
 
