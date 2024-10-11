f477f58761d804ffe9d6656c429c6accdba478d8
Caleb <kalebsam@hotmail.com>
2024-10-11 09:44:04 -0500

    fixes url2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
     url_api: 'https://backalos.kalos.icu/api/v1/',
     clientIdQr: '171572853823-p295mq0rruako4scmdfi02357g7oa6ki.apps.googleusercontent.com',
     clientIdLogin: '171572853823-v3aj7n8k98v4o4cm4e4oc2hi9h60od1f.apps.googleusercontent.com',
-    urlFront: "https://digitech.kalos.icu/"
+    urlFront: "https://beautytech.kalos.icu/"
 };
