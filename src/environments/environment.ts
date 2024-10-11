ecc90a2abd23d255abe48b0722bc9bd10c0b387c
LAMUX <lamed.saenz@gmail.com>
2024-10-10 23:17:55 -0500

    actualizacion para login con google v1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
     production: false,
     name:'dev',
     url_api: 'https://devback.bellezaap.com/api/v1/',
-    clientId: '171572853823-9iiq4tr1ffe40814bgbhfpothau7qbli.apps.googleusercontent.com',
-    clientIdLogin: '171572853823-73d98vspqp24egu8vg9no01qa55nqrhh.apps.googleusercontent.com'
+    clientIdQr: '171572853823-9iiq4tr1ffe40814bgbhfpothau7qbli.apps.googleusercontent.com',
+    clientIdLogin: '171572853823-73d98vspqp24egu8vg9no01qa55nqrhh.apps.googleusercontent.com',
     //url_api: 'http://localhost:3000/api/v1/',
 };
 
