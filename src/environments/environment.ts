4b47943c8d4f77ba437d701eecea4d09be3e3faa
LAMUX <lamed.saenz@gmail.com>
2024-10-12 18:49:08 -0500

    Ajuste para COnfiguracion de Registro por Google V2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
     production: false,
     name:'dev',
+    urlFront: "https://bellezaap.com/",
     url_api: 'https://devback.bellezaap.com/api/v1/',
     clientIdQr: '171572853823-to9tfe6gosnlek0ervd7psc4v1s2udl0.apps.googleusercontent.com',
-    clientIdLogin: '171572853823-73d98vspqp24egu8vg9no01qa55nqrhh.apps.googleusercontent.com',
-    urlFront: "https://bellezaap.com/",
+    // clientIdQr: '171572853823-lanh08801hvfga2ld6qo832738ujdrfa.apps.googleusercontent.com', // Client ID Local
     // urlFront: "http://localhost:4200/",
     // url_api: 'http://localhost:3000/api/v1/',
 };
