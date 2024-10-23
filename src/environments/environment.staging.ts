471eac69a604fa1dc35cb047f8873b77d223190b
LAMUX <lamed.saenz@gmail.com>
2024-10-23 10:41:45 -0500

    Ajuste de ClientID de Google v5

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,9 +3,9 @@ export const environment = {
     name:'staging',
     clientIdQr: "171572853823-to9tfe6gosnlek0ervd7psc4v1s2udl0.apps.googleusercontent.com", // ClientID PROD
     clientIdLogin:"171572853823-to9tfe6gosnlek0ervd7psc4v1s2udl0.apps.googleusercontent.com",
-    // url_api: 'https://devback.bellezaap.com/api/v1/',
-    // urlFront: "https://bellezaap.com/",
-    urlFront: "http://localhost:4200/",
-    url_api: 'http://localhost:3000/api/v1/',
+    url_api: 'https://devback.bellezaap.com/api/v1/',
+    urlFront: "https://bellezaap.com/",
+    // urlFront: "http://localhost:4200/",
+    // url_api: 'http://localhost:3000/api/v1/',
 };
 
