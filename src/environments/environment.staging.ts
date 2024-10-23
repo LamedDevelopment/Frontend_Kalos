df20706707b2c5b9cc4adba023bc00f7c96fd9ce
LAMUX <lamed.saenz@gmail.com>
2024-10-23 01:27:22 -0500

    Proceso de Registro y Login de Google y Facebook V9

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,10 +1,8 @@
 export const environment = {
     production: false,
     name:'staging',
-    clientIdQr: '171572853823-p295mq0rruako4scmdfi02357g7oa6ki.apps.googleusercontent.com',
-    clientIdLogin: '171572853823-v3aj7n8k98v4o4cm4e4oc2hi9h60od1f.apps.googleusercontent.com',
-    // clientIdQr: '171572853823-to9tfe6gosnlek0ervd7psc4v1s2udl0.apps.googleusercontent.com',
-    // clientIdLogin: '171572853823-73d98vspqp24egu8vg9no01qa55nqrhh.apps.googleusercontent.com',
+    clientIdQr: '171572853823-to9tfe6gosnlek0ervd7psc4v1s2udl0.apps.googleusercontent.com',
+    clientIdLogin: '171572853823-73d98vspqp24egu8vg9no01qa55nqrhh.apps.googleusercontent.com',
     url_api: 'https://devback.bellezaap.com/api/v1/',
     urlFront: "https://bellezaap.com/",
     //urlFront: "http://localhost:4200/",
